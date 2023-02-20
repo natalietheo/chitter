a56099339461bdfbb4bc4cce9a1dd5f05e47e759
Naomi Starr <naomi.starr.mail@gmail.com>
2023-02-20 12:57:57 +0000

    tailwind and daisy ui added to setup

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
     return (
-        <div className="home">
+        <div className= "text-3xl font-bold underline">
             <h2>Home</h2>
+            <p>Hi Natalie!</p>
+            <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button className="btn">Button</button>
+  <button className="btn btn-primary">Button</button>
         </div>
     )
 }
