ecbb610ce775c5b44186766ce84531701ae1e08c
Naomi Starr <naomi.starr.mail@gmail.com>
2023-03-02 11:48:12 +0000

    signup page styling - aliging items

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -1,15 +1,13 @@
 const SignupForm = () => {
   return (
-  <div className="absolute inset-x-0 top-20 m-8">
-      <div className="text-3xl">
-        <form className="flex-col flex items-center margin" >
+      <div >
+        <form className="m-8 p-8">
           <input placeholder="Name" id="name" type='text' className="input input-bordered input-primary w-full max-w-xs" /><br/>
           <input placeholder="Email" id="email" type='text' className="input input-bordered input-primary w-full max-w-xs"  /><br/>
           <input placeholder="Password" id="password" type='password' className="input input-bordered input-primary w-full max-w-xs" /><br/>
           <input id='submit' type="submit" className="input input-bordered input-primary w-full max-w-xs"   />
         </form>
       </div>
-    </div>
 
 
 
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -2,8 +2,14 @@ import SignupForm from "../components/SignupForm";
 
 const Signup = () => {
   return (
-    <div>
-      <SignupForm />
+    <div className="content-center">
+      <div className="flex-col flex items-center m-8">
+        <div className="text-3xl">
+          <h1 className="p-8">Signup here!</h1>
+
+          <SignupForm />
+        </div>
+      </div>
     </div>
   );
 };
