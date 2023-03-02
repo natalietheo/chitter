e38a86b50838b0d5f7feb60217850c73f54ba76e
Naomi Starr <naomi.starr.mail@gmail.com>
2023-03-02 11:28:06 +0000

    styling added to form

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -1,12 +1,14 @@
 const SignupForm = () => {
   return (
-    <div className="text-3xl">
-      <form className="flex-col items-center" >
-        <input placeholder="Name" id="name" type='text' className="input input-bordered input-primary w-full max-w-xs" /><br/>
-        <input placeholder="Email" id="email" type='text' className="input input-bordered input-primary w-full max-w-xs"  /><br/>
-        <input placeholder="Password" id="password" type='password' className="input input-bordered input-primary w-full max-w-xs" /><br/>
-        <input id='submit' type="submit" className="input input-bordered input-primary w-full max-w-xs"   />
-      </form>
+  <div className="absolute inset-x-0 top-20 m-8">
+      <div className="text-3xl">
+        <form className="flex-col flex items-center margin" >
+          <input placeholder="Name" id="name" type='text' className="input input-bordered input-primary w-full max-w-xs" /><br/>
+          <input placeholder="Email" id="email" type='text' className="input input-bordered input-primary w-full max-w-xs"  /><br/>
+          <input placeholder="Password" id="password" type='password' className="input input-bordered input-primary w-full max-w-xs" /><br/>
+          <input id='submit' type="submit" className="input input-bordered input-primary w-full max-w-xs"   />
+        </form>
+      </div>
     </div>
 
 
