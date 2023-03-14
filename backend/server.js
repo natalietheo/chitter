3babaca7a226584d88a3fea8ab30b3dd2d3854c2
Natalie Theodoropoulou <natalietheo@Natalies-MacBook-Air.local>
2023-03-14 10:21:00 +0000

    mongo db successfully connected

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@
 require('dotenv').config()
 
 const express = require('express');
-const mongoose = require('mongoose');
-const chitterRoutes = require('./routes/peeps')
+const chitterRoutes = require('./routes/Clucks')
 const uri = process.env.MONGO_URI
 console.log(uri);
 const port = process.env.PORT
